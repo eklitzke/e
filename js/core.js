9aebedbf2804177f16eca380e30b77bc6fc1b84e
Evan Klitzke <evan@eklitzke.org>
2012-03-31 21:29:26 -0700

    lots of JS comments

--- js/core.js
+++ js/core.js
@@ -10,6 +10,7 @@ var EventListener = require("js/event_listener.js").EventListener;
 var core = {
 	column: 0,
 	line: 0,
+	logContents: false, // when true, log the file contents after each keypress
 	curmode: "insert",
 	parser: require("js/parser.js").parser,
 	listeners: {},
@@ -336,13 +337,15 @@ core.addFunction("updateAllWindows", function (doupdate) {
 	curses.doupdate();
 });
 
-// Called when the editor is loaded
+// Called when the editor is first loaded; this loads any files specified on the
+// command line.
 world.addEventListener("load", function (event) {
 	if (world.args.length) {
 		world.buffer.open(world.args[0]);
 	}
 });
 
+// Draw and create the initial windows.
 world.addEventListener("load", function (event) {
 	core.windows = {};
 	core.windows.tab = curses.stdscr.subwin(1, curses.stdscr.getmaxx(), 0, 0);
@@ -355,6 +358,7 @@ world.addEventListener("load", function (event) {
 	core.drawStatus();
 });
 
+// Draw the buffer contents to the main window.
 world.addEventListener("load", function (event) {
 	var i = 0;
 	var maxy = core.windows.buffer.getmaxy();
@@ -370,43 +374,36 @@ world.addEventListener("load", function (event) {
 	}
 });
 
+// Move the cursor and update all windows.
 world.addEventListener("load", function (event) {
 	core.moveAbsolute(0, 0);
 	core.updateAllWindows();
 });
 
-// This method checks for Ctrl-C. We add it as its own top level handler to
-// prevent the editor from getting "stuck" due other JavaScript errors (IOW, no
-// matter what else happens you'll be able to exit using Ctrl-C).
-world.addEventListener("keypress", function (event) {
-	if (event.getCode() == 3) {
-		log("Caught Ctrl-C, stopping the main loop");
-		world.stopLoop();
-	}
-});
-
-// Core routine called on each keypress
-world.addEventListener("keypress", function (event) {
-	log("curmode is " + core.curmode);
-	switch (core.curmode) {
-	case "insert":
-		core.listeners.insert = core.listeners.insert || new EventListener();
-		core.listeners.insert.dispatch("keypress", event);
-		break;
-	default:
-		assert(false, "Unknown core.curmode \"" + core.curmode + "\"");
-		break;
-	}
-
-	// refresh the status bar
-	core.drawStatus();
-});
-
+// The absolute lowest level interface to keypresses is callbacks registered by
+// world.addEventListener("keypress", ...). That's not really a useful
+// abstraction though, because it doesn't deal with modes. The key handling for
+// the character 'x' is different if you're in vi emulation command mode, if
+// you're in insert mode, if you're typing in the minibuffer, etc. If you wanted
+// to deal with these details directly in an event handler for raw keypresses,
+// there would be a *lot* of complication caused by checking which mode you're
+// in, etc. There are also complications with the keycode abstraction provided
+// by TTYs (for instance, "meta" characters (e.g. holding down Alt while typing
+// another character) are sent as two separate characters.
+//
+// For this reason, the editor is separated into differnt "modes". An example of
+// a mode is "insert" for insert mode in the main buffer, "command" for
+// emulating vi's command mode, etc. There can be arbitrarily many modes, and
+// the current mode is set by the value of core.curmode.
+//
+// When a keypress happens, any handlers that have registered themself for the
+// mode using core.addKeypressListener will be called.
 core.addKeypressListener = function (mode, handler) {
 	core.listeners[mode] = core.listeners[mode] || new EventListener();
 	core.listeners[mode].addEventListener("keypress", handler);
 };
 
+// The main keypress listener for insert mode.
 core.addKeypressListener("insert", function (event) {
 	var curx = core.windows.buffer.getcurx();
 	var cury = core.windows.buffer.getcury();
@@ -438,7 +435,6 @@ core.addKeypressListener("insert", function (event) {
 				line.chop(core.column);
 			}
 			core.scrollRegion(-1, core.windows.buffer.getcury() + 1, core.windows.buffer.getmaxy());
-			break;
 			// add the new line, with the chopped contents
 			world.buffer.addLine(core.line + 1, chopped);
 
@@ -543,17 +539,44 @@ core.addKeypressListener("insert", function (event) {
 	}
 });
 
-/*
-world.addEventListener("keypress", function (e) {
-	log("");
-	log("<<<<<< START <<<<<<");
-	var lines = world.buffer.getContents();
-	for (var i = 0; i < lines.length; i++) {
-		log('"' + lines[i] + '"');
+// This method checks for Ctrl-C. We add it as its own top level handler to
+// prevent the editor from getting "stuck" due other JavaScript errors (IOW, no
+// matter what else happens you'll be able to exit using Ctrl-C).
+world.addEventListener("keypress", function (event) {
+	if (event.getCode() == 3) {
+		log("Caught Ctrl-C, stopping the main loop");
+		world.stopLoop();
 	}
-	log(">>>>>>> END >>>>>>>");
 });
-*/
+
+// Core routine called on each keypress; this essentially just dispatches to
+// listeners registered by core.addKeypressListener (see the comments at that
+// function for the details).
+world.addEventListener("keypress", function (event) {
+	log("curmode is " + core.curmode);
+	switch (core.curmode) {
+	case "insert":
+		core.listeners.insert = core.listeners.insert || new EventListener();
+		core.listeners.insert.dispatch("keypress", event);
+		break;
+	default:
+		assert(false, "Unknown core.curmode \"" + core.curmode + "\"");
+		break;
+	}
+
+	// refresh the status bar
+	core.drawStatus();
+
+	if (core.logContents) {
+		log("");
+		log("<<<<<< START <<<<<<");
+		var lines = world.buffer.getContents();
+		for (var i = 0; i < lines.length; i++) {
+			log('"' + lines[i] + '"');
+		}
+		log(">>>>>>> END >>>>>>>");
+	}
+});
 
 // This is the callback that specifically causes curses to flush all of its
 // drawing operations. It *must* be called after any functions that may do
@@ -561,6 +584,10 @@ world.addEventListener("keypress", function (e) {
 // it's not recommended that other functions register to listen to the
 // "after_keypress" event, and if they do they must not do any drawing
 // operations.
+//
+// It might be possible to just fold this into the main event listener for
+// "keypress" events, which users should not be hooking into. But for extra
+// safety, it's split out like this.
 world.addEventListener("after_keypress", function (e) {
 	core.updateAllWindows();
 });
