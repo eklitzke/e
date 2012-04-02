793d7229e6545d2e60df347e807888b20617d060
Evan Klitzke <evan@eklitzke.org>
2012-04-01 21:54:00 -0700

    add some getrusage() stats

--- src/main.cc
+++ src/main.cc
@@ -3,9 +3,21 @@
 #include <boost/program_options.hpp>
 #include <glog/logging.h>
 
+#ifdef USE_LINUX
+#include <sys/resource.h>
+#include <sys/time.h>
+#endif
+
+#include "./assert.h"
 #include "./curses_window.h"
 #include "./flags.h"
 #include "./state.h"
+
+#ifdef USE_LINUX
+double get_millis(const timeval &t) {
+  return (t.tv_sec * 1000000 + t.tv_usec) / 1000.0;
+}
+#endif
 
 int main(int argc, char **argv) {
   google::InitGoogleLogging(argv[0]);
@@ -25,6 +37,17 @@ int main(int argc, char **argv) {
   }
   e::CursesWindow window(scripts, files);
   window.Loop();
+#ifdef USE_LINUX
+  rusage usage;
+  ASSERT(getrusage(RUSAGE_SELF, &usage) == 0);
+  LOG(INFO) << "";
+  LOG(INFO) << "Final Resource Utilization";
+  LOG(INFO) << "--------------------------";
+  LOG(INFO) << "utime: " << get_millis(usage.ru_utime) << " ms";
+  LOG(INFO) << "stime: " << get_millis(usage.ru_stime) << " ms";
+  LOG(INFO) << "maxrss: " << (usage.ru_maxrss / 1024.0) << " MB";
+  LOG(INFO) << "";
+#endif  // USE_LINUX
   LOG(INFO) << "main() finishing with status 0";
   return 0;
 }
