2c82d588b677328a6dc83d2092a6dd2b07c6c525
Evan Klitzke <evan@eklitzke.org>
2012-04-02 09:39:22 -0700

    simplify resource usage output

--- src/main.cc
+++ src/main.cc
@@ -2,25 +2,19 @@
 
 #include <boost/program_options.hpp>
 #include <glog/logging.h>
+
 #ifdef DISPOSE_v8
 #include <v8.h>
 #endif
 
 #ifdef USE_LINUX
 #include <sys/resource.h>
-#include <sys/time.h>
 #endif
 
 #include "./assert.h"
 #include "./curses_window.h"
 #include "./flags.h"
 #include "./state.h"
-
-#ifdef USE_LINUX
-double get_millis(const timeval &t) {
-  return (t.tv_sec * 1000000 + t.tv_usec) / 1000.0;
-}
-#endif
 
 int main(int argc, char **argv) {
   google::InitGoogleLogging(argv[0]);
@@ -46,13 +40,7 @@ int main(int argc, char **argv) {
 #ifdef USE_LINUX
   rusage usage;
   ASSERT(getrusage(RUSAGE_SELF, &usage) == 0);
-  LOG(INFO) << "";
-  LOG(INFO) << "Final Resource Utilization";
-  LOG(INFO) << "--------------------------";
-  LOG(INFO) << "utime: " << get_millis(usage.ru_utime) << " ms";
-  LOG(INFO) << "stime: " << get_millis(usage.ru_stime) << " ms";
-  LOG(INFO) << "maxrss: " << (usage.ru_maxrss / 1024.0) << " MB";
-  LOG(INFO) << "";
+  LOG(INFO) << "max rss size: " << (usage.ru_maxrss / 1024) << " MB";
 #endif  // USE_LINUX
   LOG(INFO) << "main() finishing with status 0";
   return 0;
